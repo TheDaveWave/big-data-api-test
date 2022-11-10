6974f0ede23ad1214f0613a1a87da27b927938ba
David Lindberg <david.wesley.lindberg@gmail.com>
2022-11-10 13:43:28 -0600

    added geolocation promise function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 
+
 function App() {
+
+  const getPosition = () => {
+    return new Promise((resolve, reject) => 
+      navigator.geolocation.getCurrentPosition(resolve, reject)
+    );
+  }
+
   return (
     <div className="App">
       <h1>Hello World</h1>
