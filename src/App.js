82f36cc245bb267afe5452de2b2ada2edfe5408c
David Lindberg <david.wesley.lindberg@gmail.com>
2022-11-11 15:44:06 -0600

    changed to another map api

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 
 function App() {
   const [position, setPosition] = useState({});
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState({});
+  const [reverseLocation, setReverseLocation] = useState({});
+  const [gotReverse, setGotReverse] = useState(false);
   const [loading, setLoading] = useState(true);
 
 
@@ -21,18 +23,40 @@ function App() {
     await getPosition()
     .then(response => {
       const coordinates = {lat: response.coords.latitude, lng: response.coords.longitude};
-      console.log(coordinates);
+      // console.log(coordinates);
       setPosition(coordinates);
 
-      axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.lat}&longitude=${position.lng}&localityLanguage=en`)
+      // axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.lat}&longitude=${position.lng}&localityLanguage=en`)
+      // .then(response => {
+      //   console.log(response);
+      //   setCity(response.data.city);
+      //   setLoading(false);
+      // })
+      // .catch(err => {
+      //   console.log(err);
+      // });
+      axios.get(`https://nominatim.openstreetmap.org/reverse?&lat=${coordinates.lat}&lon=${coordinates.lng}&format=json`)
       .then(response => {
         console.log(response);
-        setCity(response.data.city);
+        setCity({city: response.data.address.city, state: response.data.address.state});
         setLoading(false);
       })
       .catch(err => {
         console.log(err);
       });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  const getReverseLocation = () => {
+    axios.get(`https://nominatim.openstreetmap.org/search?q=${city.city},${city.state}&format=json`)
+    // axios.get(`https://nominatim.openstreetmap.org/search?q=${city.city},ND&format=json`)
+    .then(response => {
+      console.log(response);
+      setReverseLocation({lat: response.data[0].lat, lng: response.data[0].lon});
+      setGotReverse(!gotReverse);
     })
     .catch(err => {
       console.log(err);
@@ -54,8 +78,15 @@ function App() {
         <h1>Location Bro!</h1>
         <ul>
           <li>{JSON.stringify(position)}</li>
-          <li>{city}</li>
+          <li>Location: {JSON.stringify(city)}</li>
         </ul>
+        <button onClick={() => getReverseLocation()}>Reverse Locataion</button>
+        {gotReverse &&
+          <ul>
+            <li>{JSON.stringify(reverseLocation)}</li>
+          </ul>
+        }
+        <p>Closest </p>
       </div>
     );
   }
